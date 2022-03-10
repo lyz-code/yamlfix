fdbcbf2d2bd3726fb32d89ab502b57762ac17b11
Lyz <lyz@riseup.net>
2022-03-10 18:43:40 +0100

    feat: use pdm as package manager

--- src/yamlfix/services.py
+++ src/yamlfix/services.py
@@ -1,4 +1,4 @@
-"""Gather all the orchestration functionality required by the program to work.
+"""Define all the orchestration functionality required by the program to work.
 
 Classes and functions that connect the different domain model objects with the adapters
 and handlers to achieve the program's purpose.
